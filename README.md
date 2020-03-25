4b6f770194c9162e93786e7dd5d6361cecb04b2b
unknown <hello@jeffdelaney.me>
2020-03-25 08:24:51 -0700

    reorganize code

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [Dynamic theme-toggler tutorial]() with CSS Variables and JavaScript.  
 
 
+All see [CSS Variables in 100 Seconds](https://youtu.be/NtRmIp4eMjs). 
--- public/app.js
+++ public/app.js
@@ -19,15 +19,12 @@ if (theme) {
 // Button Event Handlers
 
 darkButton.onclick = () => {
-  body.classList.remove('light');
-  body.classList.add('dark');
-
+  body.classList.replace('light', 'dark');
   localStorage.setItem('theme', 'dark');
 };
 
 lightButton.onclick = () => {
-  body.classList.remove('dark');
-  body.classList.add('light');
+  body.classList.replace('dark', 'light');
 
   localStorage.setItem('theme', 'light');
 };
